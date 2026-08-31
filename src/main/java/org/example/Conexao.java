28caf5e03cf40b25417f51d465952ef45bc7c59c
Gerson Tomaz <gersonfeto@gmail.com>
2026-08-31 11:57:04 -0300

    Add frontend and database, end main file

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public final class Conexao {
-    private static final String URL = "jbdc:mysql://localhost:3306/cantina_senai_db" +
-            "useSSL=false" +
+public final class  Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/cantina_senai_db" +
+            "?useSSL=false" +
             "&allowPublicKeyRetrieval=true" +
-            "&serverTimexone=America/Sao_Paulo";
+            "&serverTimezone=America/Sao_Paulo";
     //lalowPublicKeyRetrieval: permite que o driver JDBC solicite ao mysql uma chave publica
     //serverTimezone: informa oa Jdbc O FUSO HORÁRIO PARA TRABALHAR
     private static final String USUARIO = "cantina";
@@ -16,7 +16,7 @@ public final class Conexao {
 
     private Conexao(){}
 
-    public static final Connection conectar() throws SQLException{
+    public static Connection conectar() throws SQLException{
         return DriverManager.getConnection(URL, USUARIO, SENHA);
     }
     public static void testar() throws SQLException{
